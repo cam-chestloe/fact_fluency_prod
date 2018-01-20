ee6973c073a3a29f7f8c860bcbffb2d0910d0ec7
Fritz Jay <jonathan.jay10@gmail.com>
2018-01-19 20:28:55 -0800

    cleaned up formatting and removed skip

--- lib/fact_fluency_web/commanders/test_commander.ex
+++ lib/fact_fluency_web/commanders/test_commander.ex
@@ -1,32 +1,27 @@
 defmodule FactFluencyWeb.TestCommander do
   use Drab.Commander
-  
-  access_session [:test_id]
+
+  alias FactFluency.Testing
+
+  access_session([:test_id])
 
   def answer_question(socket, _sender) do
-    test = get_store(socket, :test, nil) || FactFluency.Testing.get_test!(get_session(socket, :test_id))
-    question = peek(socket, :question)
     index = peek(socket, :index)
     answer = peek(socket, :answer)
 
-    test = 
-    Map.update!(test, :questions, fn(questions) ->
-      List.update_at(questions, index, fn(q) ->
-        %{q | student_answer: answer}
-      end)
-    end)
+    test =
+      (index === 1 && get_store(socket, :test, nil)) ||
+        get_session(socket, :test_id) |> Testing.get_test!()
+        |> Testing.answer_question(index, answer)
 
     case Enum.fetch(test.questions, index + 1) do
       {:ok, question} ->
+        put_store(socket, :test, test)
         poke(socket, question: question, index: index + 1, answer: "")
-      :error ->
-        FactFluency.Testing.update_test(test, %{questions: test.questions, end_time: DateTime.utc_now()})
-        Drab.Browser.redirect_to(socket, "/tests/show/#{test.id}")
-      end
-  end
 
-  def skip(socket, _sender) do
-    index = peek(socket, :index)
-    poke(socket, index: index + 1)
+      :error ->
+        FactFluency.Testing.update_test(test, %{})
+        Drab.Browser.redirect_to(socket, "/tests/#{test.id}")
+    end
   end
 end
