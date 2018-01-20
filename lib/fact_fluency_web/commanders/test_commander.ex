5e1134a646b6e0aa4c8ff07ed2d5bf0b2e594ae0
Fritz Jay <jonathan.jay10@gmail.com>
2018-01-20 11:37:06 -0800

    Updated start_test and added answer_question

--- lib/fact_fluency_web/commanders/test_commander.ex
+++ lib/fact_fluency_web/commanders/test_commander.ex
@@ -4,24 +4,40 @@ defmodule FactFluencyWeb.TestCommander do
   alias FactFluency.Testing
 
   access_session([:test_id])
+ 
+  def start_test(socket, _sender) do
+    test = get_session(socket, :test_id) |> Testing.get_test!()
+      |> Testing.start_question(0)
+      #|> IO.inspect
+
+    put_store(socket, :test, test)
+
+    set_style(socket, "#answer_div", %{"display" => "block"})
+    set_style(socket, "#start_button", %{"display" => "none"})
+  end
 
   def answer_question(socket, _sender) do
+    {:ok, %{"value" => answer}} = query_one(socket, "#answer_input", "value")
     index = peek(socket, :index)
-    answer = peek(socket, :answer)
 
-    test =
-      (index === 1 && get_store(socket, :test, nil)) ||
-        get_session(socket, :test_id) |> Testing.get_test!()
-        |> Testing.answer_question(index, answer)
+    test = get_store(socket, :test, nil)
+      |> Testing.answer_question(index, answer)
+      |> IO.inspect
 
     case Enum.fetch(test.questions, index + 1) do
       {:ok, question} ->
-        put_store(socket, :test, test)
-        poke(socket, question: question, index: index + 1, answer: "")
+        
+        put_store(socket, :test, 
+          Testing.start_question(test, index + 1))
+
+        poke(socket, question: question, index: index + 1)
 
       :error ->
-        FactFluency.Testing.update_test(test, %{})
+        #IO.inspect(test.questions)
+        FactFluency.Testing.update_test(test, %{questions: test.questions})
         Drab.Browser.redirect_to(socket, "/tests/#{test.id}")
     end
+
+    set_prop(socket, "#answer_input", %{"value" => ""})
   end
 end
