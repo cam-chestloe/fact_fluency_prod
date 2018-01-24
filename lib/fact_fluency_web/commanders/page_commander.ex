195f1c7a43bd2307a5264acf42a5c4ee6153b5a8
Fritz Jay <jonathan.jay10@gmail.com>
2018-01-23 19:07:31 -0800

    Created functions to hide/show modals, and change the signup form.

--- lib/fact_fluency_web/commanders/page_commander.ex
+++ lib/fact_fluency_web/commanders/page_commander.ex
@@ -1,17 +1,32 @@
 defmodule FactFluencyWeb.PageCommander do
     use Drab.Commander
 
-    def show_modal(socket, %{"dataset" => %{"btnFor" => name}} = sender) do
-        set_style(socket, "##{name}-modal", display: "block")
+    def show_modal(socket, sender) do
+        change_modal_display(socket, sender, "block")
     end
 
-    def hide_modal(socket, %{"dataset" => %{"btnFor" => name}} = sender) do
-        set_style(socket, "##{name}-modal", display: "none")
+    def hide_modal(socket, sender) do
+        change_modal_display(socket, sender, "none")
     end
 
-    def change_signup_form(socket,  %{"value" => user_type} = sender) do
-        conn = peek(socket, "signup.html", :conn)
-        
-        set_prop(socket, "#login-form", FactFluencyWeb.PageView.render_signup_form(user_type, conn))
+    def change_modal_display(socket, name, value) do
+        case name do
+            %{"dataset" => %{"btnFor" => name}} ->
+                set_style(socket, "##{name}-modal", display: "#{value}")
+            [name, tail] ->
+                set_style(socket, "##{name}-modal", display: "#{value}")
+                change_modal_display(socket, tail, value)
+            name ->
+                set_style(socket, "##{name}-modal", display: "#{value}")
+        end
+    end
+
+    def change_signup_form(socket,  %{"value" => current_user_type} = sender) do
+        set_prop(socket, this(sender), checked: "checked")
+
+        other_user_types = ["teacher", "student", "parent"] -- [current_user_type]
+
+        show_modal(socket, current_user_type)
+        hide_modal(socket, other_user_types)
     end
 end
