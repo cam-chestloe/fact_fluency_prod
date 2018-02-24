8951ca71d5dcfc50e7d2c180d459104bfa323549
Fritz Jay <jonathan.jay10@gmail.com>
2018-02-23 20:28:19 -0800

    created parse_question in test_view

--- lib/fact_fluency_web/views/test_view.ex
+++ lib/fact_fluency_web/views/test_view.ex
@@ -2,8 +2,6 @@ defmodule FactFluencyWeb.TestView do
   use FactFluencyWeb, :view
 
   def parse_question(question, type) do
-    IO.inspect(question)
-
     [top, operator, bottom] = String.split(question)
 
     case type do
