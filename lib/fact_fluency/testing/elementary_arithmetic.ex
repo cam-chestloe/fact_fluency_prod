7339ea2fe5581c7a7c3a1b5273d8a9efdccde416
Fritz Jay <jonathan.jay10@gmail.com>
2018-02-25 12:28:59 -0800

    Handle division by zeros. Keep larger numbers on top for division and subtraction.

--- lib/fact_fluency/testing/elementary_arithmetic.ex
+++ lib/fact_fluency/testing/elementary_arithmetic.ex
@@ -35,7 +35,7 @@ defmodule FactFluency.Testing.ElementaryArithmetic do
     
     # Addition, subtraction and multiplication will feature numbers 0 - 12 (max_number?)
     # Division will feature any numbers divisible by the number up to number * 12 (max_number?)
-    featured_numbers = 
+    valid_numbers = 
       if operator == "/" do
         Enum.to_list(0..12)
         |> Enum.map(fn(num) -> num * number end)
@@ -45,8 +45,8 @@ defmodule FactFluency.Testing.ElementaryArithmetic do
 
     questions = 
       []
-      |> add_questions(featured_numbers, number, total - rand, operator)
-      |> add_random_questions(featured_numbers, total, operator)
+      |> add_questions(valid_numbers, number, total - rand, operator)
+      |> add_random_questions(valid_numbers, total, operator)
 
     questions |> Enum.map(&(%Question{question: &1}))
   end
@@ -57,61 +57,71 @@ defmodule FactFluency.Testing.ElementaryArithmetic do
   @spec grade_questions([%Question{}]) :: [%Question{}]
   def grade_questions(questions) do
     questions |> Enum.map(fn(question) ->
-      {:ok, answer} = Abacus.eval(question.question)
+
+      # handle division by zeros
+      {:ok, answer} = 
+        if String.contains?(question.question, "/") and String.contains?(question.question, "0") do
+          {:ok, 0}
+        else
+          Abacus.eval(question.question)
+        end
+
       Map.put(question, :correct_answer, to_string(answer))
     end)
   end
 
   @spec add_questions([String.t()], [integer], integer, integer, String.t()) :: [String.t()]
-  defp add_questions(questions, valid_numbers, number, total, operator) do
+  defp add_questions(questions, valid_numbers, number, total, operator) when length(questions) < total do
     length_of_questions = length(questions)
     length_of_valid_numbers = length(valid_numbers)
 
-    if length_of_questions < total do
-
-      # Add one of each `valid_numbers`
+    other_number = 
       if length_of_valid_numbers > length_of_questions do
-        questions
-          |> List.insert_at(0, "#{Enum.at(valid_numbers, length_of_questions)} #{operator} #{number}")
-          |> add_questions(valid_numbers, number, total, operator)
-      # If `questions` already contains one of each `valid_number`, add a random number from `valid_numbers`
+        Enum.at(valid_numbers, length_of_questions)
+      # If `questions` already contain one of each `valid_numbers`
+      # add a random number from `valid_numbers`
       else
-        questions
-          |> List.insert_at(0, "#{Enum.random(valid_numbers)} #{operator} #{number}")
-          |> add_questions(valid_numbers, number, total, operator)
+        Enum.random(valid_numbers)
       end
 
-    else
-      questions
-    end
+    # Keep larger numbers on top, for division and subtraction questions.
+    question = 
+      if (operator == "/" or operator == "-") and other_number > number do
+        "#{other_number} #{operator} #{number}"
+      else
+        "#{number} #{operator} #{other_number}"
+      end
+
+    questions
+      |> List.insert_at(0, question)
+      |> add_questions(valid_numbers, number, total, operator)
+  end
+
+  @spec add_questions([String.t()], [integer], integer, integer, String.t()) :: [String.t()]
+  defp add_questions(questions, _valid_numbers, _number, _total, _operator) do questions end
+
+  @spec add_random_questions([String.t()], [integer], integer, String.t()) :: [String.t()]
+  defp add_random_questions(questions, valid_numbers, total, operator) when length(questions) < total do
+    num1 = 
+      if operator == "/" do
+        Enum.random(0..12)
+      else
+        Enum.random(valid_numbers)
+      end
+    
+    num2 = 
+      case operator do
+        "/" -> num1 * Enum.random(1..12)
+        "-" -> if num1 < 12, do: Enum.random(num1..12), else: 12
+        _ -> Enum.random(valid_numbers)
+      end
+
+    List.insert_at(questions, 0, "#{num2} #{operator} #{num1}")
+    |> add_random_questions(valid_numbers, total, operator)
   end
 
   @spec add_random_questions([String.t()], [integer], integer, String.t()) :: [String.t()]
-  defp add_random_questions(questions, valid_numbers, total, operator) do
-    if length(questions) < total do
-      # Warning: Hacky code.
-      num1 = 
-        if operator == "/" do
-          Enum.random(Enum.to_list(0..12))
-        else
-          Enum.random(valid_numbers)
-        end
-      
-      # Warning: Hacky code.
-      num2 = 
-        if operator == "/" do
-          num1 * Enum.random(Enum.to_list(0..12))
-        else
-          Enum.random(valid_numbers)
-        end
-
-      List.insert_at(questions, 0, "#{num2} #{operator} #{num1}")
-      |> add_random_questions(valid_numbers, total, operator)
-      
-    else 
-      questions 
-    end
-  end
+  defp add_random_questions(questions, _valid_numbers, _total, _operator) do questions end
 
   @spec validate_arguments(map()) :: :ok
   defp validate_arguments(%{number: number, operator: operator}) do
